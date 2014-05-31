9ec59136ca9586e57c5a001a46b5441f870f2cf7
Gahzayah <fjubuu@gmail.com>
2014-05-31 13:34:43 +0200

    Update and testing

--- public_html/js/upload.js
+++ public_html/js/upload.js
@@ -9,14 +9,13 @@ var data_ = null;
 
 
 $(function() {
-
     $(btnAdd).click(function() {
         $(this).parent().find(btnFileType).click();
     });
 
     $(form).fileupload({
         url: 'http://localhost:8084/Sandbox/databasetest',
-        async: false,
+        async: true,
         type: 'POST',
         cache: false,
         dataType: 'json',
@@ -25,7 +24,7 @@ $(function() {
 
 
         add: function(e, data) {
-            // e.preventDefault()       Keine Weiterleitung
+             // Form Is not submitted in Chrome and IE.. for firefox it submits the from to same url that i am in.
 
             var filetype = data.files[0].type.split('/');
             var filename = checkFileName(data.files[0].name, filetype, filenameLength);
@@ -43,11 +42,19 @@ $(function() {
 
             if ($(selectedOutput).children().length !== 0) {
                 $(btnUpload).prop('type', 'button');
+                $("input[name='uploadXhr']").prop('type', 'button');
             }
 
             $(btnUpload).click(function() {
                 data.submit();
             });
+
+            $("input[name='uploadXhr']").click(function() {
+                xhrFunc(data);
+            });
+
+
+
         },
         done: function(e, data) {
             // $('#ajaxResponse').html('<p>Finish</p>');
@@ -55,13 +62,50 @@ $(function() {
     });
 });
 
+$("input[name='uploadXhr']").click(function() {
+    xhrFunc();
+});
+
+function xhrFunc(data) {
+
+    var http = new XMLHttpRequest();
+    var formData = new FormData();
+    var file = data;
+    
+    var url = "http://localhost:8084/Sandbox/databasetest";
+    var params = "lorem=ipsum&name=binny"; // String Pair Value
+    
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    http.setRequestHeader("Content-length", params.length);
+    http.setRequestHeader("Connection", "close");
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+        if (http.status === 200) {
+            alert(http.responseText);
+        }
+    };
+   
+
+    
+    formData.append("thefile", file);
+    http.send(formData);
+
+}
+
+
 function formatFileSize(bytes) {
     if (typeof bytes !== 'number') {
-        return '';}
+        return '';
+    }
     if (bytes >= 1000000000) {
-        return (bytes / 1000000000).toFixed(2) + ' GB';}
+        return (bytes / 1000000000).toFixed(2) + ' GB';
+    }
     if (bytes >= 1000000) {
-        return (bytes / 1000000).toFixed(2) + ' MB'; }
+        return (bytes / 1000000).toFixed(2) + ' MB';
+    }
     return (bytes / 1000).toFixed(2) + ' KB';
 }
 
